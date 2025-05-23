3df1d6220a82fa23516f6e5657a019479c36913a
Christian Ingermann <christian-manuel.ingermann@stud.uni-bamberg.de>
2025-05-23 02:57:46 +0200

    form?!

--- src/svelte-pages/contact.svelte
+++ src/svelte-pages/contact.svelte
@@ -1,15 +1,10 @@
-<form class="mb-4" name="contact" method="POST" netlify>
-    <div class="mb-3">
-        <label for="name" class="form-label">Name</label>
-        <input id="name" name="name" class="form-control" required/>
-    </div>
-    <div class="mb-3">
-        <label for="email" class="form-label">E-Mail</label>
-        <input id="email" name="email" type="email" class="form-control" required/>
-    </div>
-    <div class="mb-3">
-        <label for="message" class="form-label">Nachricht</label>
-        <textarea id="message" name="text" class="form-control" rows="4" required></textarea>
-    </div>
-    <button type="submit" class="btn btn-primary">Absenden</button>
+<form name="netlify-form-example" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+    <input type="hidden" name="form-name" value="netlify-form-example" />
+    <label for="name">Name</label>
+    <input name="name" id="name" required placeholder="Name" type="text" />
+    <label for="email">Email</label>
+    <input name="email" id="email" required placeholder="Email" type="email" />
+    <label for="message">Message</label>
+    <input name="message" id="message" required placeholder="Message" type="text" />
+    <input type="submit" value="Submit" />
 </form>
